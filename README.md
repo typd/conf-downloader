7e7f2e10fabc98c5a5663f18b0879545981cc556
typd <othertang@gmail.com>
2014-05-11 19:18:10 +0800

    support checking downloaded files robustly

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ conf-downloader/run.py -n [conf-name] -u [rss-url]
 
 e.g. 
 conf-downloader/run.py -n pycon-ca-2013 -u http://pyvideo.org/category/42/pycon-ca-2013/rss
+conf-downloader/run.py -n pycon-us-2014 -u http://pyvideo.org/category/50/pycon-us-2014/rss
--- conf-downloader/run.py
+++ conf-downloader/run.py
@@ -32,13 +32,28 @@ def main():
     folder = os.path.join(DATA_ROOT, conf)
     if not os.path.exists(folder): 
         os.makedirs(folder)
+    exist_files = files_in(folder)
+    logger.info('----------------')
+    logger.info('downloaded files:')
+    logger.info('\n'.join(exist_files))
+    logger.info('----------------')
     index = 0
     for item in items:
         index += 1
-        download_talk(logger, folder, item, index)
+        download_talk(logger, folder, exist_files, item, index)
     logger.info('================')
 
-def download_talk(logger, folder, item, index):
+def files_in(dir):
+    files = []
+    for f_or_d in os.listdir(dir):
+        path = os.path.join(dir, f_or_d)
+        if os.path.isdir(path):
+            files += files_in(path)
+        else:
+            files += [f_or_d]
+    return files
+
+def download_talk(logger, folder, exist_files, item, index):
     title = get_text(get_child_node(item, 'title'))
     enclosure = get_child_node(item, 'enclosure')
     url = enclosure.getAttribute('url')
@@ -51,10 +66,10 @@ def download_talk(logger, folder, item, index):
             title.replace(' ', '_').replace(os.path.sep, '-'), '-') +\
             '.' + vtype
     path = os.path.join(folder, file_name)
-    logger.info('path : {0}'.format(path))
-    if os.path.exists(path):
+    if file_name in exist_files:
         logger.info('  skip as file is downloaded already')
     else:
+        logger.info('  path : {0}'.format(path))
         logger.info('  downloading from {0}'.format(url))
         """
         args = ['youtube-dl', '-o', path, '"{0}"'.format(url)]
